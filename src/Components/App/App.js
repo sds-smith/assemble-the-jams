0c04522ee9741d910a51821b23b1515d69253089
Shawn Smith <sds.smith24@gmail.com>
2022-02-13 14:35:59 -0500

    creates savePlaylist method to save to user's account

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -56,6 +56,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this)
     this.removeTrack = this.removeTrack.bind(this)
     this.updatePlaylistName = this.updatePlaylistName.bind(this)
+    this.savePlaylist = this.savePlaylist.bind(this)
   }
   
   addTrack(track) {
@@ -77,6 +78,10 @@ class App extends React.Component {
     this.setState({ playlistName : name })
   }
 
+  savePlaylist() {
+    let trackURIs = this.state.playlistTracks.map(track => `spotify:track:${track.id}`)
+  }
+
   render()  {
     return (
       <div>
@@ -91,7 +96,8 @@ class App extends React.Component {
               playlistName={this.state.playlistName} 
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
-              onNameChange={this.updatePlaylistName}/>
+              onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}/>
           </div>
         </div>
       </div>

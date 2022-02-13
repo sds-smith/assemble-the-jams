264bc1577ea170eeba3af9c752d475a3892f6903
Shawn Smith <sds.smith24@gmail.com>
2022-02-13 11:59:22 -0500

    Adds addTrack method to App.js, passes addTrack through series of components to Track

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -52,8 +52,20 @@ class App extends React.Component {
           }            
         ]
     }
+
+    this.addTrack = this.addTrack.bind(this)
   }
   
+  addTrack(track) {
+    let tracks = this.state.playlistTracks
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return
+    }
+    tracks.push(track)
+    this.setState({playlistTracks : tracks})
+
+  }
+
   render()  {
     return (
       <div>
@@ -61,7 +73,9 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults}/>
+            <SearchResults 
+              searchResults={this.state.searchResults}
+              onAdd={this.addTrack}/>
             <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
           </div>
         </div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,17 +2,29 @@ import React from 'react'
 import './Track.css'
 
 class Track extends React.Component {
-    // renderAction() {
-        // isRemoval ? <button className ="Track-action">-</button> : <button className ="Track-action">+</button>
-    // }
+    constructor(props) {
+        super(props)
+
+        this.addTrack = this.addTrack.bind(this)
+    }
+    renderAction() {
+        isRemoval ? <button className ="Track-action">-</button> : 
+                    <button className ="Track-action" onClick={this.addTrack}>+</button>
+    }
+
+    addTrack() {
+        this.props.onAdd(this.props.track)
+    }
+
     render() {
         return (
             <div className="Track">
-              <div className="Track-information">
+              <div className="Track-information" isRemoval={this.props.isRemoval}>
                 <h3>{this.props.track.name}</h3>
                 <p>{this.props.track.artist} | {this.props.track.album}</p>
+                {this.renderAction()}
               </div>
-              {/* {this.renderAction()} */}
+              
             </div>            
         )
     }

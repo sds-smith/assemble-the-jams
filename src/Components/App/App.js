63a4d6ab8dae3e9efaf7c969578bc15f46c58115
Shawn Smith <sds.smith24@gmail.com>
2022-02-13 09:31:15 -0500

    creates components, adds css and img files, sets state in App component with one property, searchResults. passes down search results and renders result list

---
+++ src/Components/App/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar.js'
+import SearchResults from '../SearchResults/SearchResults.js'
+import Playlist from '../Playlist/Playlist.js'
+import React from 'react';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+        searchResults : [
+          {
+           name : 'track1',
+           artist : 'track1.artist',
+           album : 'track1.album',
+           id : 'track1.id'
+          },            
+          {
+            name : 'track2',
+            artist : 'track2.artist',
+            album : 'track2.album',
+            id : 'track2.id'
+          },          
+          {
+            name : 'track3',
+            artist : 'track3.artist',
+            album : 'track3.album',
+            id : 'track3.id'
+          }            
+        ]
+    }
+  }
+  
+  render()  {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults}/>
+            <Playlist />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;

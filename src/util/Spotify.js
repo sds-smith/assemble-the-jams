b5f84945fcd1a7585570e9980a730a65e79c80e9
Shawn Smith <sds.smith24@gmail.com>
2022-03-10 14:12:49 -0500

    updates getAccessToken

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -8,18 +8,21 @@ const Spotify = {
         if (accessToken) {
             return accessToken
         }
-        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/)
-        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/)
-        if (accessTokenMatch && expiresInMatch) {        
-            accessToken = accessTokenMatch[1]
-            const expiresIn = Number(expiresInMatch[1])
-            window.setTimeout(() => accessToken = '', expiresIn * 1000)
-            window.history.pushState("Access Token", null, "/")
-            return accessToken                 
-        } else {
+        let accessTokenMatch = window.location.href.match(/access_token=([^&]*)/)
+        let expiresInMatch = window.location.href.match(/expires_in=([^&]*)/)
+        if (!accessTokenMatch && expiresInMatch) { 
             const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`
             window.location = accessUrl
-        }
+            accessTokenMatch = window.location.href.match(/access_token=([^&]*)/)
+            expiresInMatch = window.location.href.match(/expires_in=([^&]*)/)
+
+                
+        } 
+        accessToken = accessTokenMatch[1]
+        const expiresIn = Number(expiresInMatch[1])
+        window.setTimeout(() => accessToken = '', expiresIn * 1000)
+        window.history.pushState("Access Token", null, "/")
+        return accessToken  
     },
 
     search(term) {

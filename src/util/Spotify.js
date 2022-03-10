c559d3d95ed2582e71b08feb30ab14bcc75ffdd1
Shawn Smith <sds.smith24@gmail.com>
2022-03-10 14:19:06 -0500

    y

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -10,19 +10,18 @@ const Spotify = {
         }
         let accessTokenMatch = window.location.href.match(/access_token=([^&]*)/)
         let expiresInMatch = window.location.href.match(/expires_in=([^&]*)/)
-        if (!accessTokenMatch && !expiresInMatch) { 
+        if (accessTokenMatch && expiresInMatch) { 
+            accessToken = accessTokenMatch[1]
+            const expiresIn = Number(expiresInMatch[1])
+            window.setTimeout(() => accessToken = '', expiresIn * 1000)
+            window.history.pushState("Access Token", null, "/")
+            return accessToken                  
+        } else {
             const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`
             window.location = accessUrl
-            accessTokenMatch = window.location.href.match(/access_token=([^&]*)/)
-            expiresInMatch = window.location.href.match(/expires_in=([^&]*)/)
-
-                
-        } 
-        accessToken = accessTokenMatch[1]
-        const expiresIn = Number(expiresInMatch[1])
-        window.setTimeout(() => accessToken = '', expiresIn * 1000)
-        window.history.pushState("Access Token", null, "/")
-        return accessToken  
+            return Spotify.getAccessToken()
+        }
+ 
     },
 
     search(term) {

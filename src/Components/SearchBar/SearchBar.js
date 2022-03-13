f7e1e0021a5a90f573d00540f3f7ad77498ea8fd
Shawn Smith <sds.smith24@gmail.com>
2022-03-13 10:50:07 -0400

    m

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -30,16 +30,16 @@ class SearchBar extends React.Component {
               <input placeholder="Enter A Song, Album, or Artist"  id='search_input'onChange={this.handleTermChange}/>
               <ul className='search_by'>
                     <li>
-                        <input type='checkbox' id='song' name='song'/>
-                        <label for='song'>SEARCH BY SONG</label>
+                        <input className='search_by_input' type='checkbox' id='song' name='song'/>
+                        <label className='search_by_input' for='song'>SEARCH BY SONG</label>
                     </li>
                     <li>
-                        <input type='checkbox' id='album' name='album'/>
-                        <label for='album'>SEARCH BY ALBUM</label>
+                        <input className='search_by_input' type='checkbox' id='album' name='album'/>
+                        <label className='search_by_input' for='album'>SEARCH BY ALBUM</label>
                     </li>
                     <li>
-                        <input type='checkbox' id='artist'name='album'/>
-                        <label for='artist'>SEARCH BY ARTIST</label>
+                        <input className='search_by_input' type='checkbox' id='artist'name='album'/>
+                        <label className='search_by_input' for='artist'>SEARCH BY ARTIST</label>
                     </li>
               </ul>
               <button className="SearchButton" onClick={this.search}>SEARCH</button>

cdb142b64809783cb630dd2e326ddfd452e8f3a8
Shawn Smith <sds.smith24@gmail.com>
2022-03-20 12:27:28 -0400

    removes target _blank in login screen

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -9,7 +9,7 @@ class Login extends React.Component {
                 <h2 className='loginMessage'>Assemble the Jams is registered in Development mode with Spotify.</h2>
                 <h2 className='loginMessage'>In order to use the app, you must be registered as a user.</h2>
                 <h3 className='loginMessage'>For access, please complete the form below. You will be notified by email when your request has been processed.</h3>
-                <h3 className='loginMessage'>For more information on this app, please see the <a id='readme' href='https://github.com/sds-smith/assemble-the-jams#readme' target='_blank'>README</a></h3>
+                <h3 className='loginMessage'>For more information on this app, please see the <a id='readme' href='https://github.com/sds-smith/assemble-the-jams#readme' >README</a></h3>
                 <div className='LoginForm' /*onSubmit={this.props.onLogin}*/>
                     {/* <label for='emailInput'>Enter email address associated with your Spotify account</label> */}
                     <input className='email_input' id='emailInput' type='email' placeholder='Spotify email'/>

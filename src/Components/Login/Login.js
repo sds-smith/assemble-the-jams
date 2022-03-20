0bed30f5e80ef2894db9008af4b785070264e764
Shawn Smith <sds.smith24@gmail.com>
2022-03-20 10:02:10 -0400

    a

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -11,7 +11,8 @@ class Login extends React.Component {
                 <h3 className='loginMessage'>You only need to request access once, but there may be a delay between requesting and gaining access</h3>
                 <h3 className='loginMessage'>For more information on this app, please see the About page</h3>
                 <form className='LoginForm' onSubmit={this.props.onLogin}>
-                    <input className='email_input' type='email' placeholder='Enter the email address associated with your Spotify account'/>
+                    <label for='emailInput'>Enter email address associated with your Spotify account</label>
+                    <input className='email_input' id='emailInput' type='email' placeholder='Spotify email'/>
                     <div className='regBtn'>
                         <input type='radio' name='registration' id='not_reg' value='not_reg' />
                         <label for='not_reg' id='notRegLabel'>I am requesting access for the first time</label>

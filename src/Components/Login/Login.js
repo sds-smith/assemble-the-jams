9d64ec32fdf9d456fd35b8de169f662330292039
Shawn Smith <sds.smith24@gmail.com>
2022-03-20 08:09:16 -0400

    a

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,9 +6,9 @@ class Login extends React.Component {
     render() {
         return (
             <div className='Login'>
-                <h2>This app is registered in Development mode with Spotify. In order to use the app, you must be explicitly added as a user.</h2>
-                <h3>You only need to request access once, but there may be a delay between requesting and gaining access</h3>
-                <h3>For more information on this app, please see the About page</h3>
+                <h2 className='loginMessage'>This app is registered in Development mode with Spotify. In order to use the app, you must be explicitly added as a user.</h2>
+                <h3 className='loginMessage'>You only need to request access once, but there may be a delay between requesting and gaining access</h3>
+                <h3 className='loginMessage'>For more information on this app, please see the About page</h3>
                 <input className='email_input' type='email' placeholder='Enter the email address associated with your Spotify account'/>
                 <input type='radio' name='registration' id='not_reg' value='not_reg' />
                 <label for='not_reg'>I am requesting access for the first time</label>

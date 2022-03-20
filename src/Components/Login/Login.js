3a1f58dc92c0f2f311b819536cbf7cfd8539a173
Shawn Smith <sds.smith24@gmail.com>
2022-03-20 08:05:32 -0400

    adjust margins in login component

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,10 +6,10 @@ class Login extends React.Component {
     render() {
         return (
             <div className='Login'>
-                <h3>This app is registered in Development mode with Spotify. In order to use the app, you must be explicitly added as a user.</h3>
+                <h2>This app is registered in Development mode with Spotify. In order to use the app, you must be explicitly added as a user.</h2>
                 <h3>You only need to request access once, but there may be a delay between requesting and gaining access</h3>
-                <h4>For more information on this app, please see the About page</h4>
-                <input type='email' placeholder='Enter the email address associated with your Spotify account'/>
+                <h3>For more information on this app, please see the About page</h3>
+                <input className='email_input' type='email' placeholder='Enter the email address associated with your Spotify account'/>
                 <input type='radio' name='registration' id='not_reg' value='not_reg' />
                 <label for='not_reg'>I am requesting access for the first time</label>
                 <input type='radio' name='registration' id='reg' value='reg' />

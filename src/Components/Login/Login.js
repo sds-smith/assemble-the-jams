197772bce1e9e7771dcd6896b615d757506ddd09
Shawn Smith <sds.smith24@gmail.com>
2022-03-20 14:07:33 -0400

    netlify attribute on form

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -24,7 +24,7 @@ class Login extends React.Component {
                 <h2 className='loginMessage'>In order to use the app, you must be registered as a user.</h2>
                 <h3 className='loginMessage'>For access, please complete the form below. You will be notified by email when your request has been processed.</h3>
                 <h3 className='loginMessage'>For more information on this app, please see the <a id='readme' href='https://github.com/sds-smith/assemble-the-jams#readme' >README</a></h3>
-                <form className='LoginForm' data-netlify='true' onSubmit={this.handleSubmit}>
+                <form className='LoginForm' netlify onSubmit={this.handleSubmit}>
                     {/* <label for='emailInput'>Enter email address associated with your Spotify account</label> */}
                     <input className='email_input' id='emailInput' type='email' placeholder='Spotify email'/>
                     <div className='regBtn'>

1bf375dee950372b76c0f810b2bb4df6811ae21a
Shawn Smith <sds.smith24@gmail.com>
2022-03-21 11:17:33 -0400

    converts PopUp into a stateful form component

--- src/Components/PopUp/PopUp.js
+++ src/Components/PopUp/PopUp.js
@@ -5,8 +5,21 @@ class PopUp extends React.Component {
     constructor(props) {
         super(props)
 
+        this.state = {
+          name : '',
+          email: '',
+        }
+
         this.handleClick = this.handleClick.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
+        this.encode = this.encode.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    encode(data) {
+      return Object.keys(data)
+          .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+          .join("&");
     }
 
     handleClick() {
@@ -15,14 +28,28 @@ class PopUp extends React.Component {
 
     handleSubmit(e) {
       e.preventDefault()
-      const userEmail = e.target.email.value
-      this.props.setUserEmail(userEmail)
+
+      fetch('/', {
+        method: 'POST',
+        headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        body: this.encode({ "form-name": "registration", ...this.state })
+      })
+        .then(() => {
+          const userEmail = e.target.email.value
+          this.props.setUserEmail(userEmail)
+        })
+        .catch(error => alert(error))
     }
   
+    handleChange(e) {
+      this.setState({ [e.target.name]: e.target.value });
+    } 
+
     render() {
       let formVisibility
       let registrationMessage
-
+      const { name, email } = this.state
+      
       if (this.props.userEmail) {
         formVisibility = 'hidden'
         registrationMessage = `Thank you. Your request has been submitted. You will be notified at ${this.props.userEmail} when your request has been processed.`
@@ -40,10 +67,10 @@ class PopUp extends React.Component {
             </span>
             <div className='modal_main'>
               <h2 >{registrationMessage}</h2>
-              <form className='registration' name='registration' method='POST' style={{visibility : formVisibility}} onSubmit={this.handleSubmit}>
+              <form className='registration' style={{visibility : formVisibility}} onSubmit={this.handleSubmit}>
                 <input type="hidden" name="form-name" value="registration" />
-                <input type='text' name='name' id='name' placeholder='Your First and Last Name' />
-                <input type='email' name='email' id='email' placeholder='Your Spotify email' />
+                <input type='text' name='name' id='name' value={name} placeholder='Your First and Last Name' onChange={this.handleChange}/>
+                <input type='email' name='email' id='email' value={email} placeholder='Your Spotify email' onChange={this.handleChange}/>
                 <button className='RegButton'type='submit' name='submit' id='emailSubmit' >SUBMIT</button>
               </form>
             </div>

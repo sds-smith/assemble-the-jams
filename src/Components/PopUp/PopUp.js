b777958072f8ea89f683807dad17d8f4159eed35
Shawn Smith <sds.smith24@gmail.com>
2022-03-21 09:18:50 -0400

    updates hidden html form

--- src/Components/PopUp/PopUp.js
+++ src/Components/PopUp/PopUp.js
@@ -40,9 +40,9 @@ class PopUp extends React.Component {
             </span>
             <div className='modal_main'>
               <h2 >{registrationMessage}</h2>
-              <form className='registration' name='registration' method='post' value='registration' style={{visibility : formVisibility}} onSubmit={this.handleSubmit}>
+              <form className='registration' name='registration' method='post' style={{visibility : formVisibility}} onSubmit={this.handleSubmit}>
 
-                <input type="hidden" name="form-name" value="loginForm" />
+                <input type="hidden" name="form-name" value="registration" />
                 <input type='text' name='name' id='name' placeholder='Your First and Last Name' />
                 <input type='email' name='email' id='email' placeholder='Your Spotify email' />
                 <button className='RegButton'type='submit' name='submit' id='emailSubmit' >SUBMIT</button>

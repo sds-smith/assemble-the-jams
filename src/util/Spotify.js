5963587a3790375b97009b25d146fd149cbf1b70
Shawn Smith <sds.smith24@gmail.com>
2022-03-24 12:45:02 -0400

    b

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -39,6 +39,7 @@ const Spotify = {
             .then((response) => response.json())
             .then((data) => {
                 console.log(data)
+                window.location = data.accessURL
             })
 
 

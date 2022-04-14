9d7078943328b8050b0d7a377b3c5e2c8a9f933a
Shawn Smith <sds.smith24@gmail.com>
2022-04-14 15:23:30 -0400

    copy SearchResults dir & files as Recommendations

---
+++ src/Components/Recommendations/Recommendations.js
@@ -0,0 +1,19 @@
+import React from "react";
+import TrackList from '../TrackList/TrackList'
+import './Recommendations.css'
+
+class Recommendations extends React.Component {
+    render() {
+        return (
+            <div className="Recommendations">
+              <h2>Recommendations</h2>
+              <TrackList 
+                tracks={this.props.recommendations}
+                onAdd={this.props.onAdd}
+                isRemoval={false}/>
+            </div>
+        )
+    }
+}
+
+export default Recommendations

24ac795e433274a9e3a530f49bde42395b830bbe
Shawn Smith <sds.smith24@gmail.com>
2022-04-17 19:50:08 -0400

    update redirects

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,9 +1,9 @@
 
 
 let accessToken
-//const clientId = process.env.REACT_APP_CLIENT_ID
-// const redirectURI = process.env.REACT_APP_REDIRECT_URI_NETLIFY
-const redirectURI = process.env.REACT_APP_REDIRECT_URI_LOCALHOST
+const clientId = process.env.REACT_APP_CLIENT_ID
+const redirectURI = process.env.REACT_APP_REDIRECT_URI_NETLIFY
+// const redirectURI = process.env.REACT_APP_REDIRECT_URI_LOCALHOST
 //const scope = process.env.REACT_APP_ORIGINAL_SCOPE
 
 
@@ -36,7 +36,7 @@ const Spotify = {
         if (this.parseAccessToken()) { 
             return this.parseAccessToken()                  
         } else {
-            window.location = 'https://accounts.spotify.com/authorize?client_id=21ba29fa4306450ea39e854698160967&response_type=token&scope=playlist-modify-public&redirect_uri=http://localhost:3000/'
+            window.location = '/authorize'
             }             
     },
 

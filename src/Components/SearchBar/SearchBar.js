0664cf6ecae3539c501106641bd602b6e30c4037
Shawn Smith <sds.smith24@gmail.com>
2022-04-17 15:56:22 -0400

    add p tags to slider inputs

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -32,21 +32,27 @@ class SearchBar extends React.Component {
                 <h3>Recommendation Tuner</h3>
                 <label for='acousticness'>acousticness
                     <input type='range'className='slider' name='acousticness' id='acousticness' min={0} max={1} step={0.01} defaultValue={0.5}></input>
+                    <p>1</p>
                 </label>
                 <label for='danceability'>danceability
                     <input type='range'className='slider' name='danceability' id='danceability' min={0} max={1} step={0.01} defaultValue={0.5}></input>
+                    <p>1</p>
                 </label>
                 <label for='instrumentalness'>instrumentalness
                     <input type='range'className='slider' name='instrumentalness' id='instrumentalness' min={0} max={1} step={0.5} defaultValue={0.5}></input>
+                    <p>1</p>
                 </label>
                 <label for='energy'>energy
                     <input type='range'className='slider' name='energy' id='energy' min={0} max={1} step={0.01} defaultValue={0.5}></input>
+                    <p>1</p>
                 </label>
                 <label for='liveness'>liveness
                     <input type='range'className='slider' name='liveness' id='liveness' min={0} max={1} step={0.5} defaultValue={0.5}></input>
+                    <p>1</p>
                 </label>
                 <label for='tempo'>tempo
                     <input type='range'className='slider' name='tempo' id='tempo' min={0} max={1} step={0.01} defaultValue={0.5}></input>
+                    <p>1</p>
                 </label>
               </form>
               <button className="SearchButton" onClick={this.search}>SEARCH</button>

5e57956398194f8eeb18d8588e644c1f2a5c4bbe
Shawn Smith <sds.smith24@gmail.com>
2022-04-17 15:45:44 -0400

    move recommendations tuner into searchbar comp

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -27,8 +27,28 @@ class SearchBar extends React.Component {
     render() {
         return (
             <div className="SearchBar" onKeyPress={(e) => e.key === 'Enter' && this.search()}>
-              <input placeholder="Enter A Song, Album, or Artist"  id='search_input'onChange={this.handleTermChange}/>
-
+              <input placeholder="Enter A Song, Album, or Artist"  className='searchbar_input' id='search_input'onChange={this.handleTermChange}/>
+              <form className='rec-tuner'>
+                <h3>Recommendation Tuner</h3>
+                <label for='acousticness'>acousticness
+                    <input type='range'className='slider' name='acousticness' id='acousticness' min={0} max={1} step={0.01} defaultValue={0.5}></input>
+                </label>
+                <label for='danceability'>danceability
+                    <input type='range'className='slider' name='danceability' id='danceability' min={0} max={1} step={0.01} defaultValue={0.5}></input>
+                </label>
+                <label for='instrumentalness'>instrumentalness
+                    <input type='range'className='slider' name='instrumentalness' id='instrumentalness' min={0} max={1} step={0.5} defaultValue={0.5}></input>
+                </label>
+                <label for='energy'>energy
+                    <input type='range'className='slider' name='energy' id='energy' min={0} max={1} step={0.01} defaultValue={0.5}></input>
+                </label>
+                <label for='liveness'>liveness
+                    <input type='range'className='slider' name='liveness' id='liveness' min={0} max={1} step={0.5} defaultValue={0.5}></input>
+                </label>
+                <label for='tempo'>tempo
+                    <input type='range'className='slider' name='tempo' id='tempo' min={0} max={1} step={0.01} defaultValue={0.5}></input>
+                </label>
+              </form>
               <button className="SearchButton" onClick={this.search}>SEARCH</button>
             </div>
         )

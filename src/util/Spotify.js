d71ed95d4081ed9245bfe0963053c8d0b31e9a41
Shawn Smith <sds.smith24@gmail.com>
2022-04-23 08:11:56 -0400

    comment

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -2,7 +2,7 @@
 
 let accessToken
 // const redirectURI = 'http://localhost:3000/'
-const clientId = process.env.REACT_APP_CLIENT_ID
+//const clientId = process.env.REACT_APP_CLIENT_ID
 const redirectURI = process.env.REACT_APP_REDIRECT_URI_NETLIFY
 // const redirectURI = process.env.REACT_APP_REDIRECT_URI_LOCALHOST
 //const scope = process.env.REACT_APP_ORIGINAL_SCOPE

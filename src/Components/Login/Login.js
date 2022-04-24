67e19ea9c76cede1e62f049423915afe06963093
Shawn Smith <sds.smith24@gmail.com>
2022-04-24 14:16:03 -0400

    style login and registration

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -20,20 +20,23 @@ class Login extends React.Component {
     render() {
         return (
             <div className='Login'>
-                <h2 className='loginMessage'>Assemble<span className='highlight'>the</span>Jams is registered in Development mode with Spotify.</h2>
-                <h2 className='loginMessage'>In order to demo the app, you must be registered as a user.</h2>
-                <h3 className='loginMessage'>For more information on this app, please see the <a id='readme' href='https://github.com/sds-smith/assemble-the-jams#readme' >README</a></h3>
-                <form className='LoginForm' /*method='post'*/ name='loginForm' onSubmit={this.handleSubmit}>
-                    <div className='regBtn'>
-                        <input type='radio' name='registrationStatus' id='not_reg' value='not_reg' />
-                        <label for='not_reg' id='notRegLabel'>I am requesting access for the first time</label>
-                    </div>
-                    <div className='regBtn'>
-                        <input type='radio' name='registrationStatus' id='reg' value='reg' />
-                        <label for='reg' id='regLabel'>I am a registered user of this app</label>     
-                    </div>
-                    <button className='LoginButton' >SUBMIT </button>
-                </form>
+                <div className='formContainer'>
+                    <h2 className='loginMessage'>Assemble<span className='highlight'>the</span>Jams is registered in Development mode with Spotify.</h2>
+                    <h2 className='loginMessage'>In order to demo the app, you must be registered as a user.</h2>
+                    <h3 className='loginMessage'>For more information on this app, please see the <a id='readme' href='https://github.com/sds-smith/assemble-the-jams#readme' >README</a></h3>
+                    <form className='LoginForm' /*method='post'*/ name='loginForm' onSubmit={this.handleSubmit}>
+                        <div className='regBtn'>
+                            <input type='radio' name='registrationStatus' id='not_reg' value='not_reg' />
+                            <label for='not_reg' id='notRegLabel'>I am requesting access for the first time</label>
+                        </div>
+                        <div className='regBtn'>
+                            <input type='radio' name='registrationStatus' id='reg' value='reg' />
+                            <label for='reg' id='regLabel'>I am a registered user of this app</label>     
+                        </div>
+                        <button className='LoginButton' >SUBMIT </button>
+                    </form>
+                </div>
+
             </div>
         )
     }

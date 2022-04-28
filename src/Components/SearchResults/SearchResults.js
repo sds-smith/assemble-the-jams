2c49d2bb9b4baf94e346ea968b588a3a4506cb18
Shawn Smith <sds.smith24@gmail.com>
2022-04-28 12:04:13 -0400

    column styling (height)

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -7,11 +7,14 @@ class SearchResults extends React.Component {
         return (
             <div className="SearchResults">
               <h2>Search Results</h2>
-              <TrackList 
-                tracks={this.props.searchResults}
-                onPlay={this.props.onPlay}
-                onAdd={this.props.onAdd}
-                isRemoval={false}/>
+              <div className='trackList-container'>
+                <TrackList 
+                  tracks={this.props.searchResults}
+                  onPlay={this.props.onPlay}
+                  onAdd={this.props.onAdd}
+                  isRemoval={false}/>
+              </div>
+
             </div>
         )
     }

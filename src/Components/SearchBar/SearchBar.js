48d1bfd00d4d6f0d52c2a6b38ebd66961ea2cc32
Shawn Smith <sds.smith24@gmail.com>
2022-04-28 10:49:42 -0400

    style rec tuner

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -40,30 +40,49 @@ class SearchBar extends React.Component {
               <input placeholder="Enter A Song, Album, or Artist"  className='searchbar_input' id='search_input'onChange={this.handleTermChange}/>
               <form className='rec-tuner'>
                 <h3>Customize Your Recommendations</h3>
-                <label >acousticness
-                    <input type='range'className='slider' name='acousticness' id='acousticness' min={0} max={1} step={0.01} value={this.state.acousticness} onChange={this.handleSlide}></input>
-                    <p>{this.state.acousticness}</p>
-                </label>
-                <label >danceability
-                    <input type='range'className='slider' name='danceability' id='danceability' min={0} max={1} step={0.01} value={this.state.danceability} onChange={this.handleSlide}></input>
-                    <p>{this.state.danceability}</p>
-                </label>
-                <label >instrumental
-                    <input type='range'className='slider' name='instrumentalness' id='instrumentalness' min={0} max={1} step={0.5} value={this.state.instrumentalness} onChange={this.handleSlide}></input>
-                    <p>{this.state.instrumentalness}</p>
-                </label>
-                <label >energy
-                    <input type='range'className='slider' name='energy' id='energy' min={0} max={1} step={0.01} value={this.state.energy} onChange={this.handleSlide}></input>
-                    <p>{this.state.energy}</p>
-                </label>
-                <label >live
-                    <input type='range'className='slider' name='liveness' id='liveness' min={0} max={1} step={0.5} value={this.state.liveness} onChange={this.handleSlide}></input>
-                    <p>{this.state.liveness}</p>
-                </label>
-                <label >tempo
-                    <input type='range'className='slider' name='tempo' id='tempo' min={0} max={1} step={0.01} value={this.state.tempo} onChange={this.handleSlide}></input>
-                    <p>{this.state.tempo}</p>
-                </label>
+                <div className="sliders-box">
+                    <div className='slider-container'>
+                        <label >acousticness
+                            <input type='range'className='slider' name='acousticness' id='acousticness' min={0} max={1} step={0.01} value={this.state.acousticness} onChange={this.handleSlide}></input>
+                        </label>
+                        <p className='level'>{(Math.round(this.state.acousticness * 10)/10).toFixed(1)}</p>
+                    </div>
+
+                    <div className='slider-container'>
+                        <label >danceability
+                            <input type='range'className='slider' name='danceability' id='danceability' min={0} max={1} step={0.01} value={this.state.danceability} onChange={this.handleSlide}></input>
+                        </label>
+                        <p className='level'>{(Math.round(this.state.danceability * 10)/10).toFixed(1)}</p>
+                    </div>
+
+                    <div className='slider-container'>
+                        <label >instrumental
+                            <input type='range'className='slider' name='instrumentalness' id='instrumentalness' min={0} max={1} step={0.5} value={this.state.instrumentalness} onChange={this.handleSlide}></input>
+                        </label>
+                        <p className='level'>{(Math.round(this.state.instrumentalness * 10)/10).toFixed(1)}</p>
+                    </div>
+
+                    <div className='slider-container'>
+                        <label >energy
+                            <input type='range'className='slider' name='energy' id='energy' min={0} max={1} step={0.01} value={this.state.energy} onChange={this.handleSlide}></input>
+                        </label>
+                        <p className='level'>{(Math.round(this.state.energy * 10)/10).toFixed(1)}</p>
+                    </div>
+
+                    <div className='slider-container'>
+                    <label >live
+                            <input type='range'className='slider' name='liveness' id='liveness' min={0} max={1} step={0.5} value={this.state.liveness} onChange={this.handleSlide}></input>
+                        </label>
+                        <p className='level'>{(Math.round(this.state.liveness * 10)/10).toFixed(1)}</p>
+                    </div>
+
+                    <div className='slider-container'>
+                        <label >tempo
+                            <input type='range'className='slider' name='tempo' id='tempo' min={0} max={1} step={0.01} value={this.state.tempo} onChange={this.handleSlide}></input>
+                        </label>
+                        <p className='level'>{(Math.round(this.state.tempo * 10)/10).toFixed(1)}</p>
+                    </div>
+                </div>
               </form>
               <button className="SearchButton" onClick={this.search}>SEARCH</button>
             </div>

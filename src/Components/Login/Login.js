e0bac897af82dea6ed25b6f65cf295be88b09fc0
Shawn Smith <sds.smith24@gmail.com>
2022-05-01 15:11:11 -0400

    login message

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -29,7 +29,7 @@ class Login extends React.Component {
             <div className='Login'>
                 <div className='formContainer'>
                     <h2 className='loginMessage'>Assemble<span className='highlight'>the</span>Jams is registered in Development mode with Spotify.</h2>
-                    <h2 className='loginMessage'>In order to demo the app, you must be registered as a user.</h2>
+                    <h2 className='loginMessage'>In order to use the app, Spotify requires you to be added to the list of users.</h2>
                     <h3 className='loginMessage'>For more information on this app, please see the <a id='readme' href='https://github.com/sds-smith/assemble-the-jams#readme' >README</a></h3>
                     <form 
                         className='LoginForm' 
@@ -38,11 +38,11 @@ class Login extends React.Component {
                     >
                         <div className='regBtn'>
                             <input type='radio' name='registrationStatus' id='not_reg' value='not_reg' />
-                            <label for='not_reg' id='notRegLabel'>I am requesting access for the first time</label>
+                            <label for='not_reg' id='notRegLabel'>Please check this box if you are a first-time user.</label>
                         </div>
                         <div className='regBtn'>
                             <input type='radio' name='registrationStatus' id='reg' value='reg' />
-                            <label for='reg' id='regLabel'>I am a registered user of this app</label>     
+                            <label for='reg' id='regLabel'>Registered users check this box.</label>     
                         </div>
                         <button 
                             className='LoginButton' 

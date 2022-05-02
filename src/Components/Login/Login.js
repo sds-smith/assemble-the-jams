156dad82202fb19232afd18f34cd14cf4df80669
Shawn Smith <sds.smith24@gmail.com>
2022-05-02 14:04:52 -0400

    login verbage

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -31,7 +31,7 @@ class Login extends React.Component {
                     <h2 className='loginMessage' >Assemble<span className='highlight'>the</span>Jams works with your Spotify Premium account.</h2>
                     <div className = 'rollup' style={{display : btnDisplay}}>
                             <h3 className='loginMessage'>Once you authorize the app, it can be used to search for songs, artists, or albums, receiving search term matches as well as recommendations based on the Spotify algorithm.  You can even customize how your recommendations are delivered to you using a slider toolbar.</h3>
-                            <h3 className='loginMessage'>Select the play button inside any track to hear it in the custom WebPlayer. Inside the WebPlayer, you can like/unlike the song (saves to your Spotify profile) or select a link to listen on Spotify.</h3>
+                            <h3 className='loginMessage'>Select the play button inside any track to hear it in the custom WebPlayer. Inside the WebPlayer, you can like/unlike the song (instantly saves to your Spotify profile) or select a link to listen on Spotify.</h3>
                             <h3 className='loginMessage'>Clicking on your profile picture or username in the app takes you to your Spotify profile page.</h3>
                             <h3 className='loginMessage'>This companion app is registered in Development mode with Spotify.</h3>
                             <h3 className='loginMessage'>In order to use the app, Spotify requires you to be added to the list of users.</h3>

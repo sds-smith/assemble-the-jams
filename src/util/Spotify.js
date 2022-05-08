de35864d48ce188e8cac0619a07e1e2dcfd76bd4
Shawn Smith <sds.smith24@gmail.com>
2022-05-08 12:28:25 -0400

    xyz

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -246,3 +246,6 @@ const Spotify = {
 }
 
 export default Spotify
+
+//[build] 
+//command = "sed -i s/REACT_APP_AUTHORIZE_REDIRECT_PLACEHOLDER/$REACT_APP_AUTHORIZE_REDIRECT/g netlify.toml && npm run build"

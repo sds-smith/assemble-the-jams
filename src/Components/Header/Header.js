f84711868ed91b0802e831af540f514d22475814
Shawn Smith <sds.smith24@gmail.com>
2022-05-09 14:56:11 -0400

    add Header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import '../App/App.css'
+import SpotifyLogo from '../../icons/Spotify_Logo_RGB_White.png'
+
+class Header extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <div className='spotify-attributor'>
+                  <img src={SpotifyLogo} id='spotify-logo' alt='Spotify Logo'/>
+                  <a href='https://www.spotify.com/us/premium/' target='_blank' rel="noreferrer"><p>Works with Spotify Premium</p></a> 
+                </div>        
+                <h1>Assemble<span className="highlight">the</span>Jams</h1>
+            </div>
+
+  
+        )
+    }
+}
+
+export default Header
